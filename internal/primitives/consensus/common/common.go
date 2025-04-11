00175f95343baae7c538ed12453a716ff9677b67
Timothy Wu <tim.wu@chainsafe.io>
2025-04-11 17:46:35 -0400

    more cleanup

--- internal/primitives/consensus/common/common.go
+++ internal/primitives/consensus/common/common.go
@@ -3,9 +3,12 @@
 
 package common
 
+// BlockOrigin is the block data origin
 type BlockOrigin uint
 
 const (
+	// NetworkInitialSync means the block is part of the initial sync with the network.
 	NetworkInitialSync BlockOrigin = iota
+	// NetworkBroadcast means the block was broadcasted on the network.
 	NetworkBroadcast
 )
