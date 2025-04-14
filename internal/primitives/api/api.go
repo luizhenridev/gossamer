8008b280e30daa01b93e18fafa225ccb9dbe9d6d
Timothy Wu <tim.wu@chainsafe.io>
2025-04-14 12:40:14 -0400

    code cleanup

--- internal/primitives/api/api.go
+++ internal/primitives/api/api.go
@@ -1,16 +1,11 @@
 package api
 
-// /// Something that provides a runtime api.
-// pub trait ProvideRuntimeApi<Block: BlockT> {
+// ProvideRuntimeAPI is the interface that provides a runtime api.
 type ProvideRuntimeAPI[API any] interface {
-	// 	/// The concrete type that provides the api.
-	// 	type Api: ApiExt<Block>;
-
-	// /// Returns the runtime api.
-	// /// The returned instance will keep track of modifications to the storage. Any successful
-	// /// call to an api function, will `commit` its changes to an internal buffer. Otherwise,
-	// /// the modifications will be `discarded`. The modifications will not be applied to the
-	// /// storage, even on a `commit`.
-	// fn runtime_api(&self) -> ApiRef<Self::Api>;
+	/// Returns the runtime api.
+	/// The returned instance will keep track of modifications to the storage. Any successful
+	/// call to an api function, will commit its changes to an internal buffer. Otherwise,
+	/// the modifications will be discarded. The modifications will not be applied to the
+	/// storage, even on a commit.
 	RuntimeAPI() API
 }
